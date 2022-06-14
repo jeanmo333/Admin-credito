187dd78f91e30c2f9784495fd23c49a09c87a070
jeanmo333 <jeanmo333@gmail.com>
2022-06-13 21:20:34 -0400

    commit in the head

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <h1 className="font-black text-3xl text-center md:w-2/3 mx-auto">
+    <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
       Seguimiento {""}
       <span className="text-indigo-700">Cliente</span>
     </h1>
